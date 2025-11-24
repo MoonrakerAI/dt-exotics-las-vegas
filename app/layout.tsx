2e1b678df94047c43f046d1426718f501d235d14
Your Name <your-email@example.com>
2025-11-24 23:38:51 +0700

    Added GTM code

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,16 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PW9T3KT5');`
+        }} />
+        {/* End Google Tag Manager */}
+        
         {/* Preload critical hero video for faster LCP */}
         <link rel="preload" href="/videos/hero/Hero Background.mp4" as="video" type="video/mp4" crossOrigin="anonymous" />
         
@@ -36,6 +46,17 @@ export default function RootLayout({
         />
       </head>
       <body className="min-h-screen antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PW9T3KT5"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         <LazyResourceLoader>
           <div className="fixed inset-0 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-metal-gray/20 via-dark-gray to-dark-gray pointer-events-none" />
           {children}
