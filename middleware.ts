35e2382ae6fd57aa3d16dc6b7ae1b953375497bd
Your Name <your-email@example.com>
2025-11-25 13:31:17 +0700

    w

--- middleware.ts
+++ middleware.ts
@@ -6,16 +6,16 @@ export async function middleware(req: NextRequest) {
   // Content Security Policy
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://maps.googleapis.com;
-    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-    font-src 'self' https://fonts.gstatic.com;
-    img-src 'self' data: blob: https: http:;
-    media-src 'self' https://b9c4kbeqsdvzvtpz.public.blob.vercel-storage.com;
-    connect-src 'self' https://api.stripe.com https://maps.googleapis.com https://api.anthropic.com;
-    frame-src 'self' https://js.stripe.com https://www.youtube.com https://www.google.com;
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://maps.googleapis.com https://www.googletagmanager.com https://*.googletagmanager.com https://www.google-analytics.com https://*.google-analytics.com https://ssl.google-analytics.com https://www.googleadservices.com https://googleads.g.doubleclick.net https://*.doubleclick.net https://www.google.com https://www.gstatic.com https://www.clarity.ms https://*.clarity.ms https://c.clarity.ms https://connect.facebook.net https://www.facebook.com https://*.googleoptimize.com;
+    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.gstatic.com;
+    font-src 'self' data: https://fonts.gstatic.com https://fonts.googleapis.com;
+    img-src 'self' data: blob: https://maps.googleapis.com https://fonts.googleapis.com https://b9c4kbeqsdvzvtpz.public.blob.vercel-storage.com https://www.googletagmanager.com https://*.googletagmanager.com https://www.google-analytics.com https://*.google-analytics.com https://ssl.google-analytics.com https://www.google.com https://www.gstatic.com https://googleads.g.doubleclick.net https://*.doubleclick.net https://www.googleadservices.com https://www.clarity.ms https://*.clarity.ms https://c.clarity.ms https://www.facebook.com https://connect.facebook.net;
+    media-src 'self' blob: data: https://b9c4kbeqsdvzvtpz.public.blob.vercel-storage.com;
+    connect-src 'self' https://api.stripe.com https://maps.googleapis.com https://api.anthropic.com https://www.googletagmanager.com https://*.googletagmanager.com https://www.google-analytics.com https://*.google-analytics.com https://ssl.google-analytics.com https://stats.g.doubleclick.net https://www.googleadservices.com https://googleads.g.doubleclick.net https://*.doubleclick.net https://www.google.com https://www.clarity.ms https://*.clarity.ms https://c.clarity.ms https://connect.facebook.net https://www.facebook.com https://graph.facebook.com;
+    frame-src 'self' https://js.stripe.com https://www.youtube.com https://www.googletagmanager.com https://*.googletagmanager.com https://www.google.com https://googleads.g.doubleclick.net https://*.doubleclick.net https://bid.g.doubleclick.net https://www.facebook.com https://connect.facebook.net;
     object-src 'none';
     base-uri 'self';
-    form-action 'self';
+    form-action 'self' https://js.stripe.com;
     frame-ancestors 'none';
     upgrade-insecure-requests;
   `.replace(/\s{2,}/g, ' ').trim()
